55bb317683223a837481e5b38306a673e26f478e
Astrid Hecht <silentaviary@gmail.com>
2022-06-08 15:27:22 -0600

    Finish decision making file

--- decision_making.rb
+++ decision_making.rb
@@ -35,9 +35,26 @@ end
 
 # Questions
 # 1. In English, using technical vocabulary, describe what is happening between lines 12 and 16.
+#      If you chose door 1, the bear tries to wear a hat. If you chose a door 2, the bear tries to wear a scarf
+
 # 2. What variable has a new value assigned to it after the first if statement executes?
+#      bear_clothing
+
 # 3. If you changed the variable door_choice to equal 3, what would the bear_clothing value be?
+#      The scarf
+
 # 4. In English, using technical vocabulary, describe what is happening between lines lines 25 and 33.
+#      The program takes your response to the choice on lines 21 - 23 and gives different scenarios depending on your answer by compariing the given value to the numbers of the options to see if they're equal. If they are, the code puts out text describing the consequences
+#      If you chose #1, the bear is greatful and shows you a secret passage.
+#      If you chose #2, you make the bear cry :(
+#      If you chose #3, you pull an Indiana Jones
+#      If you chose any value outside the given option values, the else statement catches it and you chill with the bear for all eternity
+
 # 5. If you changed the variable bear_choice to equal 3, what will be the final outcome be?
+#     Snake room. So unless you have anit-venin, probably not great.
+
 # 6. If you changed the variable door_choice to equal 1, and the variable bear_choice to equal 2, what will be the final outcome be?
+#      You cruely insult the bears headwear, you scoundrel
+
 # 7. What is your favorite ending?
+#     An eternity of bear friendship (the else statement)
